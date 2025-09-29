a475853fb3b77730135c024335d186c6e3aaee98
Moderabo <patrik@modorato.se>
2025-09-29 15:09:05 +0200

    Check not empty

--- inttest_md.py
+++ inttest_md.py
@@ -12,7 +12,7 @@ class TestMDIntegration(unittest.TestCase):
 
 	def test_run_md_creates_traj(self):
 		md.run_md()
-		self.assertTrue(os.path.exists("cu.traj"), "cu.traj file was not created")
+		self.assertTrue(os.path.getsize("cu.traj") > 0, "cu.traj file was not created")
 
 	def tearDown(self):
 		# Clean up cu.traj after test
